5e78000fa1977893aff14eadcea4b10589be42dd
root <root@ip-172-31-21-57.us-east-2.compute.internal>
2022-03-28 17:55:07 +0000

    fixed

--- user-data.sh
+++ user-data.sh
@@ -2,18 +2,20 @@
 apt-get update
 apt-get install -y apache2
 service apache2 start
-cat <<EOF >>/var/www/html/index.html
+echo "" /var/www/html/index.html
+cat <<EOT >> /var/www/html/index.html
 <!DOCTYPE html>
 
 <html>
-    <head>
-        <title>
-            First Web Page
-        </title>
-    </head>
+	<head>
+		<title>
+			First Web Page
+		</title>
+	</head>
 
-    <body>
-        Hello World!
-    </body>
+	<body>
+		Hello World!
+	</body>
 </html>
-EOF
+EOT
+
